44c83a0971ae0e6b0097e2c29902a2eac1bf0b41
JYH <verin4494@gmail.com>
2022-04-10 19:33:21 +0900

    6

--- codingTest/src/com/company/String6.java
+++ codingTest/src/com/company/String6.java
@@ -2,29 +2,29 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
-
+public class String6 {
 
     public static void main(String[] args) {
-        Main T = new Main();
+        String6 T = new String6();
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-
         String solution = T.solution(str);
         System.out.println(solution);
-
     }
 
     private String solution(String str) {
-
         String answer = "";
 
-        for (char c1 : str.toCharArray()) {
-            if (answer.indexOf(c1) == -1) {
-                answer += c1;
+        for (int i = 0; i < str.length(); i++) {
+//            System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+            if (str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
             }
         }
-        return answer;
 
+
+
+
+        return answer;
     }
 }
