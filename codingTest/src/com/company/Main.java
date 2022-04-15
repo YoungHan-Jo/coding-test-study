948b59f3bb1e177dfc998a16bca22d5d89bf3b2f
JYH <verin4494@gmail.com>
2022-04-16 01:23:34 +0900

    10

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -7,42 +7,37 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(T.solution(str));
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+
+        int[] solution = T.solution(str, c);
+        for (int i : solution) {
+            System.out.print(String.valueOf(i) + " ");
+        }
     }
 
-    private String solution(String str) {
-        String answer = "";
-
+    private int[] solution(String str, char c) {
+        int[] answer = new int[str.length()];
         char[] s = str.toCharArray();
-        int count = 1;
-
-        for (int i = 0; i < s.length-1; ++i) {
-            if (i == s.length - 2) {
-                if (s[i] == s[i + 1]) {
-                    count = count + 1;
-                    answer = answer + s[i] + count;
-                } else {
-                    if (count == 1) {
-                        answer = answer + s[i] + s[i + 1];
-                    } else {
-                        answer = answer + s[i] + count + s[i + 1];
-                    }
-                }
-                break;
-            }
-            if (s[i] == s[i + 1]) {
-                count++;
+        int p = 1000;
+        for (int i = 0; i < str.length(); ++i) {
+            if (s[i] == c) {
+                answer[i] = (p = 0);
             } else {
-                if (count == 1) {
-                    answer = answer + s[i];
-                } else {
-                    answer = answer + s[i] + count;
-                    count = 1;
-                }
+                answer[i] = ++p;
             }
         }
 
+        p = 1000;
+        for (int i = str.length() - 1; i >= 0; --i) {
+            if (s[i] == c) {
+                answer[i] = (p = 0);
+            } else {
+                if (answer[i] > ++p) {
+                    answer[i] = p;
+                }
+            }
+        }
         return answer;
     }
 
