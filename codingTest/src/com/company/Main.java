f467c4877315558afed1f3668595e1ad9bcb71b9
JYH <verin4494@gmail.com>
2022-04-19 00:55:19 +0900

    string12

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -7,26 +7,25 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+        String cnt = sc.nextLine();
         String str = sc.nextLine();
-
-        System.out.println(T.solution(str));
+        System.out.println(T.solution(Integer.valueOf(cnt), str));
     }
 
-    private String solution(String str) {
+    private String solution(int cnt, String str) {
         String answer = "";
-        str +=  " ";
-        int p = 1;
-        for (int i = 0; i < str.length()-1 ; ++i) {
-            if (str.charAt(i) == str.charAt(i + 1)) {
-                p++;
-            } else {
-                if (p == 1) {
-                    answer = answer + str.charAt(i);
-                } else {
-                    answer = answer +  str.charAt(i) + p;
+
+        for (int i = 0; i < cnt; ++i) {
+            int sum = 0;
+            String sign = str.substring(i * 7, i * 7 + 7);
+            String binary = new StringBuilder(sign).reverse().toString();
+            for (int j = 0; j < binary.length() ; ++j) {
+                if (binary.charAt(j) == '#') {
+                    int pow = (int) Math.pow(2, j);
+                    sum += pow;
                 }
-                p = 1;
             }
+            answer += (char) sum;
         }
         return answer;
     }
