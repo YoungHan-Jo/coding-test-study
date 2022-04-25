568948232f78efd6c94d2641ce4d877fed9f7e06
JYH <verin4494@gmail.com>
2022-04-26 01:11:50 +0900

    arr3

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,23 +10,37 @@ public class Main {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; ++i) {
-            arr[i] = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0 ; i < n; ++i) {
+            a[i] = sc.nextInt();
         }
-        System.out.println(T.solution(n,arr));
+        for (int i = 0 ; i < n; ++i) {
+            b[i] = sc.nextInt();
+        }
+
+        for (String s : T.solution(n, a, b)) {
+            System.out.println(s);
+        }
     }
 
-    private int solution(int n, int[] arr) {
-        int answer = 1;
-        int max = arr[0];
+    private List<String> solution(int n, int[] a, int[] b) {
+        List<String> answer = new ArrayList<>();
 
-        for (int i = 1; i < n; ++i) {
-            if (arr[i] > max) {
-                max = arr[i];
-                answer++;
+        for (int i = 0; i < n; ++i) {
+            if (a[i]==b[i]) {
+                answer.add("D");
+            } else if (a[i] == 1 && b[i] == 3) {
+                answer.add("A");
+            } else if (a[i] == 2 && b[i] == 1) {
+                answer.add("A");
+            } else if (a[i] == 3 && b[i] == 2) {
+                answer.add("A");
+            } else {
+                answer.add("B");
             }
         }
+
 
         return answer;
     }
