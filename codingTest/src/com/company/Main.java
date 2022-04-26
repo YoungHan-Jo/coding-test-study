8a22c0cec1280ec18a924ae74f67afbc2ccbcc84
JYH <verin4494@gmail.com>
2022-04-27 00:16:51 +0900

    arr4

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -10,37 +10,19 @@ public class Main {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] a = new int[n];
-        int[] b = new int[n];
-        for (int i = 0 ; i < n; ++i) {
-            a[i] = sc.nextInt();
-        }
-        for (int i = 0 ; i < n; ++i) {
-            b[i] = sc.nextInt();
-        }
-
-        for (String s : T.solution(n, a, b)) {
-            System.out.println(s);
+        for (Integer i : T.solution(n)) {
+            System.out.print(i + " ");
         }
     }
 
-    private List<String> solution(int n, int[] a, int[] b) {
-        List<String> answer = new ArrayList<>();
+    private List<Integer> solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(1);
+        answer.add(1);
 
-        for (int i = 0; i < n; ++i) {
-            if (a[i]==b[i]) {
-                answer.add("D");
-            } else if (a[i] == 1 && b[i] == 3) {
-                answer.add("A");
-            } else if (a[i] == 2 && b[i] == 1) {
-                answer.add("A");
-            } else if (a[i] == 3 && b[i] == 2) {
-                answer.add("A");
-            } else {
-                answer.add("B");
-            }
+        for (int i = 2; i < n; ++i) {
+            answer.add(answer.get(i-2) + answer.get(i-1));
         }
-
 
         return answer;
     }
---
+++ codingTest/src/com/company/array/A4.java
@@ -0,0 +1,25 @@
+package com.company.array;
+
+import java.util.Scanner;
+
+public class A4 {
+    public static void main(String[] args) {
+        A4 T = new A4();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i : T.solution(n)) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private int[] solution(int n) {
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+        for (int i = 2; i < n; ++i) {
+            answer[i] = answer[i - 2] + answer[i - 1];
+        }
+
+        return answer;
+    }
+}
