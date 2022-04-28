6bbb7db64a5c8497f2fc69bf8c2a32389cb2ca1c
JYH <verin4494@gmail.com>
2022-04-29 01:44:11 +0900

    a5

--- codingTest/src/com/company/array/A5.java
+++ codingTest/src/com/company/array/A5.java
@@ -1,11 +1,11 @@
-package com.company;
+package com.company.array;
 
 import java.util.Scanner;
 
-public class Main {
+public class A5 {
 
     public static void main(String[] args) {
-        Main T = new Main();
+        A5 T = new A5();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(T.solution(n));
@@ -15,17 +15,15 @@ public class Main {
         int answer = 0;
 
         int[] ch = new int[n + 1];
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; ++i) {
             if (ch[i] == 0) {
                 answer++;
                 for (int j = i; j <= n; j = j + i) {
                     ch[j] = 1;
                 }
             }
-
         }
 
         return answer;
     }
-
 }
