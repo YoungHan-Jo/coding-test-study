b311a9bb8eafdf852f46a51a20e7aa6585fff36d
JYH <verin4494@gmail.com>
2022-05-01 14:54:47 +0900

    a6

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,24 +10,34 @@ public class Main {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(T.solution(n));
-    }
-
-    private int solution(int n) {
-        int answer = 0;
-
-        int[] ch = new int[n + 1];
-        for (int i = 2; i < n + 1; ++i) {
-            if (ch[i] == 0) {
-                answer++;
-                for (int j = i; j < n + 1; j = j + i) {
-                    ch[j] = 1;
-                }
-            }
+        List<String> stringList = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            stringList.add(sc.next());
+        }
+        for (Integer i : T.solution(stringList)) {
+            System.out.print(i + " ");
         }
 
+    }
+
+    private List<Integer> solution(List<String> stringList) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (String str : stringList) {
+            int reverseInt = Integer.valueOf(new StringBuilder(str).reverse().toString());
+
+            if (isPrime(reverseInt)) answer.add(reverseInt);
+        }
 
         return answer;
+    }
+
+    private boolean isPrime(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) count++;
+        }
+        return count == 2? true: false;
     }
 
 }
