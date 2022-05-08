be35226db06ef00cdd43c4d8c7929b34126f2a44
JYH <verin4494@gmail.com>
2022-05-09 01:07:54 +0900

    a12

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -9,51 +11,44 @@ public class Main {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] arr = new int[n][5];
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < 5; ++j) {
+        int m = sc.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
                 arr[i][j] = sc.nextInt();
             }
         }
-
-        System.out.println(T.solution(n,arr));
+        System.out.println(T.solution(n, m, arr));
     }
 
-    private int solution(int n, int[][] arr) {
+    private int solution(int n, int m, int[][] arr) {
         int answer = 0;
-        int high = 0;
 
-        for (int i = 0; i < n; ++i) { // i 학생 번호
-            int[] same = new int[n];
+        Map<String, Integer> map = new HashMap<>();
 
-            for (int j = 0; j < 5; ++j) { // j 학년
+        for (int i = 0; i < n; ++i) {
+            for (int k = i+1; k < n; ++k) {
+                map.put(arr[0][i] + "," + arr[0][k], 0);
+            }
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int k = j+1; k < n; ++k) {
 
-                for (int k = 0; k < n; ++k) { // k 비교하는 학생
-                    if (arr[i][j] == arr[k][j]) {
-                        same[k] = 1;
+                    if (map.containsKey(arr[i][j] + "," + arr[i][k])) {
+                        map.replace(arr[i][j] + "," + arr[i][k], map.get(arr[i][j] + "," + arr[i][k])+1);
                     }
                 }
             }
+        }
 
-            int sum = sum(same);
-            if (sum > high) {
-                answer = i + 1;
-                high = sum;
+        for (Integer v : map.values()) {
+            if (v == m-1) {
+                answer++;
             }
         }
 
-
-
         return answer;
     }
-
-    private int sum(int[] same) {
-        int sum = 0;
-        for (int i = 0; i < same.length; ++i) {
-            sum += same[i];
-        }
-        return sum;
-    }
-
 
 }
