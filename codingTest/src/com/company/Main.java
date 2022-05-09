b0fa16b7a03e0df2f755576e8ff9d5f7948b9581
JYH <verin4494@gmail.com>
2022-05-10 00:30:48 +0900

    tp1

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -1,8 +1,8 @@
 package com.company;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,44 +11,41 @@ public class Main {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] arr = new int[m][n];
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                arr[i][j] = sc.nextInt();
-            }
+        int[] a = new int[n];
+        for (int i = 0; i < n; ++i) {
+            a[i] = sc.nextInt();
         }
-        System.out.println(T.solution(n, m, arr));
+        int m = sc.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; ++i) {
+            b[i] = sc.nextInt();
+        }
+
+        for (Integer i : T.solution(n, m, a, b)) {
+            System.out.print(i + " ");
+        }
     }
 
-    private int solution(int n, int m, int[][] arr) {
-        int answer = 0;
+    private List<Integer> solution(int n, int m, int[] a, int[] b) {
+        List<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
 
-        Map<String, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < n; ++i) {
-            for (int k = i+1; k < n; ++k) {
-                map.put(arr[0][i] + "," + arr[0][k], 0);
+        while (p1 < n && p2 < m) {
+            if (a[p1] < b[p2]) {
+                answer.add(a[p1++]);
+            }else{
+                answer.add(b[p2++]);
             }
         }
-        for (int i = 1; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                for (int k = j+1; k < n; ++k) {
-
-                    if (map.containsKey(arr[i][j] + "," + arr[i][k])) {
-                        map.replace(arr[i][j] + "," + arr[i][k], map.get(arr[i][j] + "," + arr[i][k])+1);
-                    }
-                }
-            }
+        while (p1 < n) {
+            answer.add(a[p1++]);
         }
-
-        for (Integer v : map.values()) {
-            if (v == m-1) {
-                answer++;
-            }
+        while (p2 < m) {
+            answer.add(b[p2++]);
         }
 
         return answer;
+
     }
 
 }
