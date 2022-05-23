25e8282d98db7a6273408592164d3e24775ce464
JYH <verin4494@gmail.com>
2022-05-23 23:43:42 +0900

    p4

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -6,39 +6,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         int m = sc.nextInt();
-        int k = sc.nextInt();
-        int[] arr = new int[m];
-        for (int i = 0; i < m; ++i) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; ++i) {
             arr[i] = sc.nextInt();
         }
-
-        System.out.println(T.solution(m, k, arr));
-
+        System.out.println(T.solution(n, m, arr));
     }
 
-    private int solution(int m, int k, int[] arr) {
-        int answer;
+    private int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int lt = 0;
+
         int sum = 0;
 
-        for (int i = 0; i < k; ++i) {
-            sum += arr[i];
-        }
-
-        answer = sum;
-
-        for (int i = k; i < m; ++i) {
-            sum += arr[i];
-            sum -= arr[i-k];
-
-            answer = Math.max(answer, sum);
+        for (int rt = 0; rt < n ; ++rt ) {
+            sum += arr[rt];
+            if (sum == m) answer++;
+            while (sum >= m) {
+                sum -= arr[lt++];
+                if(sum==m) answer++;
+            }
         }
 
 
         return answer;
     }
-
 }
