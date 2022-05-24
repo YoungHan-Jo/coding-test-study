723e14df4a5d24e670823cc39b65b30dbc810350
JYH <verin4494@gmail.com>
2022-05-24 23:21:21 +0900

    p5

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -9,27 +9,24 @@ public class Main {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; ++i) {
-            arr[i] = sc.nextInt();
-        }
-        System.out.println(T.solution(n, m, arr));
+
+        System.out.print(T.solution(n));
     }
 
-    private int solution(int n, int m, int[] arr) {
+    private int solution(int n) {
         int answer = 0;
-        int lt = 0;
+        int lt = 1;
+        int sum = 1;
+        int m = n / 2 + 1;
 
-        int sum = 0;
-
-        for (int rt = 0; rt < n ; ++rt ) {
-            sum += arr[rt];
-            if (sum == m) answer++;
-            while (sum >= m) {
-                sum -= arr[lt++];
-                if(sum==m) answer++;
+        for (int rt = 2; rt <= m; ++rt) {
+            sum += rt;
+            if(sum == n) answer++;
+            while (sum >= n && lt < rt) {
+                sum -= lt++;
+                if (sum == n) answer++;
             }
+
         }
 
 
