5422366d7e29f4010f7af217f444008476e9f464
JYH <verin4494@gmail.com>
2022-06-01 16:28:24 +0900

    string4

--- codingTest/src/com/company/ch1/String4.java
+++ codingTest/src/com/company/ch1/String4.java
@@ -1,45 +1,29 @@
 package com.company.ch1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class String4 {
 
-    private List<String> solution(List<String> list) {
-        List<String> answer = new ArrayList<>();
-
-        for (String s : list) {
-            char[] c = s.toCharArray();
-
-            int lt = 0, rt = s.length() - 1;
-
-            while (lt < rt) {
-                char temp = c[lt];
-                c[lt] = c[rt];
-                c[rt] = temp;
-                lt++;
-                rt--;
-            }
-            answer.add(String.valueOf(c));
-        }
-        return answer;
-    }
-
     public static void main(String[] args) {
         String4 T = new String4();
         Scanner sc = new Scanner(System.in);
-
-        int num = sc.nextInt();
-        List<String> list = new ArrayList<>();
-
-        for (int i = 0; i < num; i++) {
-            list.add(sc.next());
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; ++i) {
+            arr[i] = sc.next();
         }
-
-        for (String s : T.solution(list)) {
+        String[] solution = T.solution(n, arr);
+        for (String s : solution) {
             System.out.println(s);
         }
+    }
 
+    private String[] solution(int n, String[] arr) {
+
+        for (int i = 0; i < n; ++i) {
+            arr[i] = new StringBuilder(arr[i]).reverse().toString();
+        }
+
+        return arr;
     }
 }
