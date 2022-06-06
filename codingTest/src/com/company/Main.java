0730ecffb1f50e92e669deb25fbd1b86bddb589d
JYH <verin4494@gmail.com>
2022-06-06 19:08:03 +0900

    string10

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -8,20 +8,43 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(T.solution(str));
+        String str = sc.next();
+        char c = sc.next().toCharArray()[0];
+        int[] solution = T.solution(str, c);
+        for (int i : solution) {
+            System.out.print(i + " ");
+        }
+
     }
 
-    public int solution(String str) {
-        String answer = "";
+    private int[] solution(String str, char c) {
+        int[] answer = new int[str.length()];
+
         char[] chars = str.toCharArray();
 
+        int len = 100;
+
         for (int i = 0; i < chars.length; ++i) {
-            if (Character.isDigit(chars[i])) {
-                answer += chars[i];
+            if (chars[i] == c) {
+                len = 0;
+            }else{
+                len++;
             }
+            answer[i] = len;
         }
-        return Integer.valueOf(answer);
+
+        len = 100;
+
+        for (int i = chars.length - 1; i >= 0; --i) {
+            if (chars[i] == c) {
+                len = 0;
+            } else {
+                len++;
+            }
+            answer[i] = Math.min(answer[i], len);
+        }
+
+        return answer;
     }
 
 
