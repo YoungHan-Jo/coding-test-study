e3f33c0ab58dce5f3f55d3f674b67543185b4913
JYH <verin4494@gmail.com>
2022-06-11 23:28:44 +0900

    str11

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -8,42 +8,28 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        char c = sc.next().toCharArray()[0];
-        int[] solution = T.solution(str, c);
-        for (int i : solution) {
-            System.out.print(i + " ");
-        }
+        String str = sc.nextLine();
+        System.out.println(T.solution(str));
+
 
     }
 
-    private int[] solution(String str, char c) {
-        int[] answer = new int[str.length()];
-
-        char[] chars = str.toCharArray();
-
-        int len = 100;
-
-        for (int i = 0; i < chars.length; ++i) {
-            if (chars[i] == c) {
-                len = 0;
-            }else{
-                len++;
-            }
-            answer[i] = len;
-        }
-
-        len = 100;
-
-        for (int i = chars.length - 1; i >= 0; --i) {
-            if (chars[i] == c) {
-                len = 0;
+    private String solution(String str) {
+        str += ' ';
+        char[] c = str.toCharArray();
+        String answer = String.valueOf(c[0]);
+        int cnt = 1;
+        for (int i = 1; i < c.length; ++i) {
+            if (c[i] == c[i - 1]) {
+                cnt++;
             } else {
-                len++;
+                if (cnt > 1) {
+                    answer += cnt;
+                }
+                answer += String.valueOf(c[i]);
+                cnt = 1;
             }
-            answer[i] = Math.min(answer[i], len);
         }
-
         return answer;
     }
 
--- codingTest/src/com/company/ch1/String11Answer.java
+++ codingTest/src/com/company/ch1/String11Answer.java
@@ -31,4 +31,23 @@ public class String11Answer {
 
         return answer;
     }
+
+    private String solution2(String str) {
+        str += ' ';
+        char[] c = str.toCharArray();
+        String answer = String.valueOf(c[0]);
+        int cnt = 1;
+        for (int i = 1; i < c.length; ++i) {
+            if (c[i] == c[i - 1]) {
+                cnt++;
+            } else {
+                if (cnt > 1) {
+                    answer += cnt;
+                }
+                answer += String.valueOf(c[i]);
+                cnt = 1;
+            }
+        }
+        return answer;
+    }
 }
