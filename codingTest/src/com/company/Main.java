dcabef93e7da8ac12c8b7b6e5b0f35d28dc2dbf0
JYH <verin4494@gmail.com>
2022-06-25 22:02:02 +0900

    arr 1

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,29 +10,28 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String[] arr = new String[n];
-        String str = sc.next() + " ";
-        for (int i = 0; i < n; i++) {
-            arr[i] = str.substring(0,7);
-            str = str.substring(7);
+        int k = sc.nextInt();
+        int[] arr = new int[k];
+        for (int i = 0; i < k; ++i) {
+            arr[i] = sc.nextInt();
         }
-
-        System.out.println(T.solution(arr));
+        List<Integer> solution = T.solution(k, arr);
+        for (Integer i : solution) {
+            System.out.print(i + " ");
+        }
 
     }
 
-    private String solution(String[] arr) {
-        String answer = "";
+    private List<Integer> solution(int k, int[] arr) {
+        List<Integer> answer = new ArrayList<>();
 
-        for (int i = 0; i < arr.length ; i++) {
-            answer += getCharFromStringBinary(arr[i]);
+        answer.add(arr[0]);
+
+        for (int i = 1; i < k; i++) {
+            if (arr[i] > arr[i - 1]) answer.add(arr[i]);
         }
+
         return answer;
-    }
-    private char getCharFromStringBinary(String str) {
-        String replace = str.replace("#", "1").replace("*", "0");
-        return (char) (int) Integer.valueOf(replace, 2);
     }
 
 
