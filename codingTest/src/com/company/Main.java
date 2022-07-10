485d5fd7ddb95eddc3027db9558dc4a8a3b27f4f
Jo YoungHan <verin4494@gmail.com>
2022-07-10 22:04:21 +0900

    arr3

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,23 +10,45 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; ++i) {
-            arr[i] = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arrA = new int[k];
+        for (int i = 0; i < k; ++i) {
+            arrA[i] = sc.nextInt();
         }
-        System.out.println(T.solution(n,arr));
+        int[] arrB = new int[k];
+        for (int i = 0; i < k; ++i) {
+            arrB[i] = sc.nextInt();
+        }
+
+        for (String str : T.solution(k, arrA, arrB)) {
+            System.out.println(str);
+        }
+
     }
 
-    private int solution(int n, int[] arr) {
-        int count = 0;
-        int highest = Integer.MIN_VALUE;
-        for (int height : arr) {
-            if (height > highest) {
-                count++;
-                highest = height;
+    private List<String> solution(int k, int[] arrA, int[] arrB) {
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 0; i < k; ++i) {
+            int a = arrA[i];
+            int b = arrB[i];
+            String result = "";
+            if (a == b) {
+                result = "D";
+            } else {
+                if (a == 1) {
+                    result = b == 2 ? "B" : "A";
+                } else if (a == 2) {
+                    result = b == 3 ? "B" : "A";
+                } else if (a == 3) {
+                    result = b == 1 ? "B" : "A";
+                }
             }
+            answer.add(result);
         }
-        return count;
+
+        return answer;
     }
+
+
 }
