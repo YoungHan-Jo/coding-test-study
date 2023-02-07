578bbc53e7bf977a25aea083cff65d6ce9f49589
Jo YoungHan <verin4494@gmail.com>
2023-02-07 22:02:49 +0900

    string10

--- codingTest/src/com/company/ch1/String10.java
+++ codingTest/src/com/company/ch1/String10.java
@@ -1,29 +1,29 @@
-package com.company;
+package com.company.ch1;
 
 
 import java.util.Scanner;
 
-public class Main {
+public class String10 {
 
     public static void main(String[] args) {
-
-        Main T = new Main();
+        String10 T = new String10();
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        char c = sc.next().charAt(0);
+        char t = sc.next().charAt(0);
 
-        for (int i : T.solution(str, c)) {
+        for (int i : T.solution(str, t)) {
             System.out.print(i + " ");
         }
+
     }
 
-    private int[] solution(String str, char c) {
+    private int[] solution(String str, char t) {
         int[] answer = new int[str.length()];
 
         int p = 1000;
 
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == c) {
+            if (str.charAt(i) == t) {
                 p = 0;
             } else {
                 p++;
@@ -31,19 +31,15 @@ public class Main {
             answer[i] = p;
         }
 
-        p = 1000;
-
         for (int i = str.length() - 1; i >= 0; i--) {
-            if (str.charAt(i) == c) {
+            if (str.charAt(i) == t) {
                 p = 0;
             } else {
                 p++;
             }
-            answer[i] = Math.min(answer[i], p);
+            answer[i] = Math.min(p, answer[i]);
         }
 
         return answer;
     }
-
-
 }
