80841d2551804b5cea046076d363a73514294733
Jo YoungHan <verin4494@gmail.com>
2023-02-10 20:59:14 +0900

    array4

--- codingTest/src/com/company/Main.java
+++ codingTest/src/com/company/Main.java
@@ -6,37 +6,26 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
+        List<Integer> answer = T.solution(n);
+        for (Integer i : answer) {
+            System.out.print(i + " ");
+        }
+    }
 
-        List<Integer> arr = new ArrayList<>();
+    private List<Integer> solution(int n) {
+        List<Integer> answer = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            arr.add(sc.nextInt());
-        }
-
-        int answer = T.solution(arr);
-        System.out.println(answer);
-
-    }
-
-    private int solution(List<Integer> arr) {
-        int answer = 0;
-
-        int t = 0;
-
-        for (Integer i : arr) {
-            if (i > t) {
-                answer++;
-                t = i;
+            if (i == 0 || i == 1) {
+                answer.add(1);
+                continue;
             }
+            answer.add(answer.get(i - 2) + answer.get(i - 1));
         }
-
         return answer;
     }
-
 }
